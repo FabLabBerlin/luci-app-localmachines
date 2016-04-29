fe732c963c95b3047bdb592851743bd2f8a056bc
syso <git@ext.no-route.org>
2016-04-29 12:32:29 +0200

    Add LuCI-App for Local Machines.

--- luasrc/model/cbi/localmachines/localmachines.lua
+++ luasrc/model/cbi/localmachines/localmachines.lua
@@ -4,7 +4,6 @@ m = Map("localmcahines", translate("Localmachines"),
 	translate("Localmachines setup wizard"))
 
 s = m:section(TypedSection, "localmachines", translate("Server Settings"))
-s.anonymous = true
 
 s:option(Value, "jabberid", translate("Jabber-ID"),
          translate("The Local Machines JabberID assigned to you."))
